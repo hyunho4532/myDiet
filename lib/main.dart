dbc3354e88777a1e2018a7ee052dc02572e672c6
hyunho4532 <kyounguk192@gmail.com>
2025-08-17 17:42:58 +0900

    feat. 식단 관리 기능 구현 중 (supabase 연동 완료)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mydiet/presentation/widget/view/persistent_tabview.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await dotenv.load(fileName: ".env");
+
   await Supabase.initialize(
-    url: 
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_KEY']!
   );
 
   runApp(const MyApp());
