3cd2a58772ca6622a8c297fb2c5ca541c2dc1065
hyunho4532 <kyounguk192@gmail.com>
2025-08-17 17:39:04 +0900

    feat. 식단 관리 기능 구현 중 (supabase 연동 중)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:mydiet/presentation/widget/view/persistent_tabview.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 
+  );
+
   runApp(const MyApp());
 }
 
