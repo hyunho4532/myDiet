d2d68966dab6b22cbb2daeb029eeefdf46f8c122
hyunho4532 <kyounguk192@gmail.com>
2025-08-25 23:32:25 +0900

    feat. dietMessages 구현 중

--- lib/presentation/screens/diet_s.dart
+++ lib/presentation/screens/diet_s.dart
@@ -27,7 +27,7 @@ class _DietSState extends State<DietS> {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
             Obx(() {
@@ -50,49 +50,88 @@ class _DietSState extends State<DietS> {
                   )
               );
             }),
-            
+
+            // 식단 리스트 (스크롤 가능)
             Expanded(
               child: Obx(() {
                 final diets = dietController.diets
-                  .where((diet) => isSameDay(diet.foodDate, dietController.selectedDate.value)).toList();
+                    .where((diet) =>
+                    isSameDay(diet.foodDate, dietController.selectedDate.value))
+                    .toList();
 
                 return ListView.builder(
                   itemCount: diets.length,
                   itemBuilder: (context, index) {
                     final diet = diets[index];
-
-                    // foodType별 색상 지정
-                    Color typeColor;
-                    switch (diet.foodType) {
-                      case '식단':
-                        typeColor = Const().buildColors()[0];
-                        break;
-                      default:
-                        typeColor = Colors.grey;
-                    }
+                    String dietMessages = "";
 
                     final sumKcal = diet.foodList.fold(
                       0.0,
                           (sum, item) => sum += item.energyKcal,
                     );
 
+                    if (diet.foodKind == '아침') {
+                      if (sumKcal < 400) {
+                        dietMessages = "너무 적게 먹었습니다!";
+                      } else if (sumKcal >= 400 && sumKcal <= 600) {
+                        dietMessages = "적당하게 먹었습니다!";
+                      } else {
+                        dietMessages = "너무 많이 먹었습니다!";
+                      }
+                    }
+
+                    if (diet.foodKind == '점심') {
+                      if (sumKcal < 500) {
+                        dietMessages = "너무 적게 먹었습니다!";
+                      } else if (sumKcal >= 560 && sumKcal <= 840) {
+                        dietMessages = "적당하게 먹었습니다!";
+                      } else {
+                        dietMessages = "너무 많이 먹었습니다!";
+                      }
+                    }
+
+                    if (diet.foodKind == '저녁') {
+                      if (sumKcal < 480) {
+                        dietMessages = "너무 적게 먹었습니다!";
+                      } else if (sumKcal >= 480 && sumKcal <= 720) {
+                        dietMessages = "적당하게 먹었습니다!";
+                      } else {
+                        dietMessages = "너무 많이 먹었습니다!";
+                      }
+                    }
+
                     return ListTile(
                       title: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 4.0),
+                            child: Text(
+                              dietMessages,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: Colors.grey
+                              ),
+                            ),
+                          ),
+
                           for (int i = 0; i < diet.foodList.length; i += 2)
                             Row(
                               children: [
                                 for (int j = i; j < i + 2 && j < diet.foodList.length; j++)
                                   Padding(
-                                    padding: const EdgeInsets.only(
+                                    padding: EdgeInsets.only(
                                         right: 8, bottom: 4),
                                     child: SizedBox(
                                       height: 80,
                                       child: Card(
                                         child: Padding(
-                                          padding: const EdgeInsets.all(6.0),
-                                          child: Text(diet.foodList[j].foodName),
+                                          padding: EdgeInsets.all(6.0),
+                                          child: Column(
+                                            children: [
+                                              Text(diet.foodList[j].foodName),
+                                            ],
+                                          ),
                                         ),
                                       ),
                                     ),
@@ -106,7 +145,7 @@ class _DietSState extends State<DietS> {
                   },
                 );
               }),
-            )
+            ),
           ],
         ),
       )
