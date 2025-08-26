8008604d7a289e95260d79f7f04636151b4e06cd
hyunho4532 <kyounguk192@gmail.com>
2025-08-26 22:00:20 +0900

    feat. 탭 레이아웃 구현 중 (식단 item 위젯 구현 완료)

--- lib/presentation/screens/diet_s.dart
+++ lib/presentation/screens/diet_s.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:mydiet/presentation/const.dart';
 import 'package:mydiet/presentation/controller/diet_c.dart';
 import 'package:mydiet/presentation/widget/builder/builder.dart';
+import 'package:mydiet/presentation/widget/item/item.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class DietS extends StatefulWidget {
@@ -76,132 +77,8 @@ class _DietSState extends State<DietS> with TickerProviderStateMixin {
                       isSameDay(diet.foodDate, dietController.selectedDate.value))
                       .toList();
 
-                  return ListView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: diets.length,
-                    itemBuilder: (context, index) {
-                      final diet = diets[index];
-                      String dietMessages = "";
-
-                      final sumKcal = diet.foodList.fold(
-                        0.0,
-                            (sum, item) => sum += item.energyKcal,
-                      );
-
-                      if (diet.foodKind == '아침') {
-                        if (sumKcal < 400) {
-                          dietMessages = "너무 적게 먹었습니다!";
-                        } else if (sumKcal >= 400 && sumKcal <= 600) {
-                          dietMessages = "적당하게 먹었습니다!";
-                        } else {
-                          dietMessages = "너무 많이 먹었습니다!";
-                        }
-                      }
-
-                      if (diet.foodKind == '점심') {
-                        if (sumKcal < 500) {
-                          dietMessages = "너무 적게 먹었습니다!";
-                        } else if (sumKcal >= 560 && sumKcal <= 840) {
-                          dietMessages = "적당하게 먹었습니다!";
-                        } else {
-                          dietMessages = "너무 많이 먹었습니다!";
-                        }
-                      }
-
-                      if (diet.foodKind == '저녁') {
-                        if (sumKcal < 480) {
-                          dietMessages = "너무 적게 먹었습니다!";
-                        } else if (sumKcal >= 480 && sumKcal <= 720) {
-                          dietMessages = "적당하게 먹었습니다!";
-                        } else {
-                          dietMessages = "너무 많이 먹었습니다!";
-                        }
-                      }
-
-                      return ListTile(
-                          title: Card(
-                            elevation: 0,
-                            color: Colors.white,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12),
-                                side: BorderSide(
-                                    color: Colors.grey,
-                                    width: 0.5
-                                )
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(bottom: 4.0),
-                                        child: Text(
-                                          dietMessages,
-                                          style: TextStyle(
-                                              fontSize: 14,
-                                              color: Colors.grey
-                                          ),
-                                        ),
-                                      ),
-
-                                      Padding(
-                                        padding: const EdgeInsets.only(bottom: 4.0),
-                                        child: Text(
-                                          "${diet.foodDate.hour}시 ${diet.foodDate.minute}분",
-                                          style: TextStyle(
-                                              fontSize: 14,
-                                              color: Colors.grey
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-
-                                  for (int i = 0; i < diet.foodList.length; i += 3)
-                                    Row(
-                                      children: [
-                                        for (int j = i; j < i + 3 && j < diet.foodList.length; j++)
-                                          Container(
-                                            constraints: BoxConstraints(
-                                                minWidth: 100,
-                                                minHeight: 60
-                                            ),
-                                            child: Card(
-                                              color: Const().buildColors()[2],
-                                              child: Padding(
-                                                padding: EdgeInsets.all(6.0),
-                                                child: Column(
-                                                  children: [
-                                                    Text(
-                                                      diet.foodList[j].foodName,
-                                                      style: TextStyle(
-                                                          color: Colors.white
-                                                      ),
-                                                    ),
-
-                                                    Text(
-                                                      "${diet.foodList[j].energyKcal} kcal",
-                                                      style: TextStyle(
-                                                          color: Colors.white
-                                                      ),
-                                                    )
-                                                  ],
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                      ],
-                                    ),
-                                ],
-                              ),
-                            ),
-                          )
-                      );
-                    },
+                  return Item(
+                    data: diets,
                   );
                 }),
                 Card(
--- lib/presentation/widget/item/item.dart
+++ lib/presentation/widget/item/item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mydiet/presentation/const.dart';
 
 class Item<T> extends StatefulWidget {
   final List<T> data;
@@ -15,6 +16,132 @@ class Item<T> extends StatefulWidget {
 class _ItemState extends State<Item> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView.builder(
+      physics: const BouncingScrollPhysics(),
+      itemCount: widget.data.length,
+      itemBuilder: (context, index) {
+        final diet = widget.data[index];
+        String dietMessages = "";
+
+        final sumKcal = diet.foodList.fold(
+          0.0,
+              (sum, item) => sum += item.energyKcal,
+        );
+
+        if (diet.foodKind == '아침') {
+          if (sumKcal < 400) {
+            dietMessages = "너무 적게 먹었습니다!";
+          } else if (sumKcal >= 400 && sumKcal <= 600) {
+            dietMessages = "적당하게 먹었습니다!";
+          } else {
+            dietMessages = "너무 많이 먹었습니다!";
+          }
+        }
+
+        if (diet.foodKind == '점심') {
+          if (sumKcal < 500) {
+            dietMessages = "너무 적게 먹었습니다!";
+          } else if (sumKcal >= 560 && sumKcal <= 840) {
+            dietMessages = "적당하게 먹었습니다!";
+          } else {
+            dietMessages = "너무 많이 먹었습니다!";
+          }
+        }
+
+        if (diet.foodKind == '저녁') {
+          if (sumKcal < 480) {
+            dietMessages = "너무 적게 먹었습니다!";
+          } else if (sumKcal >= 480 && sumKcal <= 720) {
+            dietMessages = "적당하게 먹었습니다!";
+          } else {
+            dietMessages = "너무 많이 먹었습니다!";
+          }
+        }
+
+        return ListTile(
+            title: Card(
+              elevation: 0,
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  side: BorderSide(
+                      color: Colors.grey,
+                      width: 0.5
+                  )
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 4.0),
+                          child: Text(
+                            dietMessages,
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey
+                            ),
+                          ),
+                        ),
+
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 4.0),
+                          child: Text(
+                            "${diet.foodDate.hour}시 ${diet.foodDate.minute}분",
+                            style: TextStyle(
+                                fontSize: 14,
+                                color: Colors.grey
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    for (int i = 0; i < diet.foodList.length; i += 3)
+                      Row(
+                        children: [
+                          for (int j = i; j < i + 3 && j < diet.foodList.length; j++)
+                            Container(
+                              constraints: BoxConstraints(
+                                  minWidth: 100,
+                                  minHeight: 60
+                              ),
+                              child: Card(
+                                color: Const().buildColors()[2],
+                                child: Padding(
+                                  padding: EdgeInsets.all(6.0),
+                                  child: Column(
+                                    children: [
+                                      Text(
+                                        diet.foodList[j].foodName,
+                                        style: TextStyle(
+                                            color: Colors.white
+                                        ),
+                                      ),
+
+                                      Text(
+                                        "${diet.foodList[j].energyKcal} kcal",
+                                        style: TextStyle(
+                                            color: Colors.white
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                        ],
+                      ),
+                  ],
+                ),
+              ),
+            )
+        );
+      },
+    );
   }
 }
